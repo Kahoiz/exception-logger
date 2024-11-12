c7466c8b420e6e2907b725f11a79d20c38583cc6
Benjamin Christiansen <10461216@ucn.dk>
2024-11-12 10:01:49 +0100

    update and added config to users to edit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'database' => [
+        // Database driver (e.g., mysql, pgsql, sqlite)
+        'driver' => 'mysql',
+
+        // Database host (e.g., 127.0.0.1, localhost)
+        'host' => '',
+
+        // Database port (e.g., 3306 for MySQL)
+        'port' => '',
+
+        // Database name
+        'database' => 'your_database_name',
+
+        // Database username
+        'username' => 'your_database_username',
+
+        // Database password
+        'password' => 'your_database_password',
+    ],
+    'redis' => [
+        // TODO: Add Redis configuration
+    ]
+];
--- src/ExceptionLoggerMiddleware.php
+++ src/ExceptionLoggerMiddleware.php
@@ -2,10 +2,19 @@
 namespace kahoiz\ExceptionLogger;
 use Closure;
 
+
 class ExceptionLoggerMiddleware
 {
+    private $pdo;
+    public function __construct()
+    {
+        $config = require __DIR__ . '/../config.php';
+        $dsn = "{$config['database']['driver']}:host={$config['database']['host']};port={$config['database']['port']};dbname={$config['database']['database']}";
+        $username = $config['database']['username'];
+        $password = $config['database']['password'];
+        $this->pdo = new \PDO($dsn, $username, $password);
 
-
+    }
     public function handle($request, Closure $next)
     {
         try {
@@ -14,10 +23,21 @@ class ExceptionLoggerMiddleware
 
         } catch (\Exception $e) {
             echo("ExceptionLogger end with exception");
+            $this->logException($e);
             //return the error message
-            return $e->getMessage();
+            return $response;
         }
         echo("ExceptionLogger end");
         return $response;
+    }
+    private function logException(\Exception $e)
+    {
+        $query = $this->pdo->prepare('INSERT INTO exception_logs (message, file, line, trace) VALUES (:message, :file, :line, :trace)');
+        $query->execute([
+            ':message' => $e->getMessage(),
+            ':file' => $e->getFile(),
+            ':line' => $e->getLine(),
+            ':trace' => $e->getTraceAsString(),
+        ]);
     }
 }
