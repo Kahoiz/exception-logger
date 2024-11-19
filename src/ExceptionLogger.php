08f91800c5d84979b4a5038596ac3d4daca8300e
Benjamin Christiansen <10461216@ucn.dk>
2024-11-19 12:22:07 +0100

    Cooked down to only pushing json to the queue defined by the application

---
+++ src/ExceptionLogger.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace kahoiz\ExceptionLogger;
+
+use Closure;
+use Illuminate\Support\Facades\Queue;
+
+
+class ExceptionLogger
+{
+
+
+    public function handle($request, Closure $next)
+    {
+
+        $response = $next($request);
+
+        if(!$response->exception) {
+
+            return $response;
+        }
+
+        //QUEUE
+        Queue::pushRaw(json_encode([
+            'type' => get_class($response->exception),
+            'message' => $response->exception->getMessage(),
+            'file' => $response->exception->getFile(),
+            'line' => $response->exception->getLine(),
+            'trace' => $response->exception->getTraceAsString(),
+            'sessionuid' => $request->session()->getId(),
+            'environment' => env("APP_NAME"),
+            'thrown_at' => now()
+        ], JSON_THROW_ON_ERROR), 'new-exception');
+
+        //EVENT
+//        $data = [
+//            'type' => get_class($response->exception),
+//            'message' => $response->exception->getMessage(),
+//            'file' => $response->exception->getFile(),
+//            'line' => $response->exception->getLine(),
+//            'trace' => $response->exception->getTraceAsString(),
+//            'sessionuid' => $request->session()->getId(),
+//            'environment' => env("APP_NAME"),
+//            'thrown_at' => now()
+//        ];
+//        NewException::dispatch($data);
+        return $response;
+
+    }
+
+}
