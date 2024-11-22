44b8f2a20d228ab57554c91b3a9f8a692390c3a7
Benjamin Christiansen <10461216@ucn.dk>
2024-11-22 12:40:27 +0100

    Actual unique uuid

--- src/ExceptionLogger.php
+++ src/ExceptionLogger.php
@@ -4,6 +4,7 @@ namespace kahoiz\ExceptionLogger;
 
 use Closure;
 use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Str;
 
 
 class ExceptionLogger
@@ -19,31 +20,18 @@ class ExceptionLogger
 
             return $response;
         }
-
-        //QUEUE
+        //In laravel 8, pushRaw expects a string as the first argument, so we'll have to encode the array to a json string
         Queue::pushRaw(json_encode([
             'type' => get_class($response->exception),
             'message' => $response->exception->getMessage(),
             'file' => $response->exception->getFile(),
             'line' => $response->exception->getLine(),
             'trace' => $response->exception->getTraceAsString(),
-            'sessionuid' => $request->session()->getId(),
+            'uuid' => (string) Str::uuid(),
             'environment' => env("APP_NAME"),
             'thrown_at' => now()->format('Y-m-d H:i:s')
         ], JSON_THROW_ON_ERROR), 'new-exception');
 
-        //EVENT
-//        $data = [
-//            'type' => get_class($response->exception),
-//            'message' => $response->exception->getMessage(),
-//            'file' => $response->exception->getFile(),
-//            'line' => $response->exception->getLine(),
-//            'trace' => $response->exception->getTraceAsString(),
-//            'sessionuid' => $request->session()->getId(),
-//            'environment' => env("APP_NAME"),
-//            'thrown_at' => now()
-//        ];
-//        NewException::dispatch($data);
         return $response;
 
     }
